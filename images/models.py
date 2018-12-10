ed7dc9cc6108cc871b044ec264907b8aa9e2d32b
Ghost Rider <dianamulungi@gmail.com>
2018-12-10 16:36:22 +0300

    database tables created through classes,supposed to store data assigned to it

--- images/models.py
+++ images/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=41)
+    description = models.TextField()
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.order_by()
+        return images
+
+    @classmethod
+    def get_image(cls, id):
+        image = cls.objects.get(id=id)
+        return image
+
+    @classmethod
+    def filter_by_category(cls, id):
+        images = cls.objects.filter(category_id=id)
+        return images
+
+    @classmethod
+    def filter_by_location(cls,id):
+        images = cls.objects.filter(location_id=id)
+        return images
+
+
+    @classmethod
+    def search_image(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
