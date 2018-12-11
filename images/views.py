d01da3dc54e785f8df47fb38533f9f5e16f20240
Ghost Rider <dianamulungi@gmail.com>
2018-12-11 12:26:28 +0300

    function for filtering an image by location

--- images/views.py
+++ images/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Image,Location,Category
 #................
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def filter_by_location(request,location):
+    '''
+    Filters the database and displays images according to location_id
+    '''
+    location = Location.objects.get(name = location)
+    images = Image.filter_by_location(location.id)
+    return render(request, 'location.html', {"images": images})
