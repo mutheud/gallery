e9eca5b513021f13dc5214ad72cd1ba0b0cf87b0
Ghost Rider <dianamulungi@gmail.com>
2018-12-11 12:22:45 +0300

    media root for images added

--- photos/settings.py
+++ photos/settings.py
@@ -64,11 +64,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
-
 WSGI_APPLICATION = 'photos.wsgi.application'
 
 
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
