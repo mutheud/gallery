6ffb7b204a3f4ffa5cbfe0b4acf5a541cbfe8b12
Ghost Rider <dianamulungi@gmail.com>
2018-12-13 14:47:19 +0300

    function for displaying all images in a welcome.html template

--- images/views.py
+++ images/views.py
@@ -3,7 +3,8 @@ from .models import Image,Location,Category
 #................
 # Create your views here.
 def welcome(request):
-    return render(request, 'welcome.html')
+    images = Image.objects.all()
+    return render(request, 'welcome.html', {"images": images})
 
 def filter_by_location(request,location):
     '''
@@ -12,3 +13,21 @@ def filter_by_location(request,location):
     location = Location.objects.get(name = location)
     images = Image.filter_by_location(location.id)
     return render(request, 'location.html', {"images": images})
+
+def filter_by_category(request, category):
+    category = Category.objects.get(name = category)
+    images = Image.filter_by_category(category.id)
+    return render(request, 'category.html',{"images":images})
+
+def search_image(request):
+  
+    if 'image' in request.GET and request.GET["image"]:
+        category = request.GET.get("image")
+        searched_images = Image.search_image(category)
+        message = f"{category}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = ".You haven't searched for any category"
+        return render(request, 'search.html',{"message":message})
